ec5fb929312148d6b31eddf5cadf2f3fda969a5b
Alex Finnarn <alex.finnarn@gmail.com>
2022-05-16 21:58:21 -0400

    remove tailwindcss

--- web/themes/cua_giving/js/cua-giving.js
+++ web/themes/cua_giving/js/cua-giving.js
@@ -3,15 +3,10 @@
  * CUA Giving behaviors.
  */
 (function (Drupal) {
-
   'use strict';
-
   Drupal.behaviors.cuaGiving = {
     attach: function (context, settings) {
-
       console.log('It works!');
-
     }
   };
-
-} (Drupal));
+}(Drupal));
