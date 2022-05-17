50a66255d0d93bda2cc9a8733a87e467e03ed90b
Alex Finnarn <alex.finnarn@gmail.com>
2022-05-16 20:18:44 -0400

    add a sub theme

---
+++ web/themes/cua_giving/js/cua-giving.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * CUA Giving behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.cuaGiving = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
