7660cb0dbdfd7e36521380d7e8b54118c2f24df1
Alex Finnarn <alex.finnarn@gmail.com>
2022-06-11 13:40:26 -0400

    more styling of the homepage

--- web/sites/default/settings.ddev.php
+++ web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'KqadpgvOrRGQvZXqsZqIVcJcogSeVwCIGRjSCQJGXNLoTRfSXIWMBXwSHOIlAaUV';
+$settings['hash_salt'] = 'VZFbEvEuPQyiPioadyxGnOYpWMnsZRbtjVyeoViQbMTGLdWximeZPCOZozbSoqBK';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
